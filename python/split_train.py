0ad572f5494c5eb95281dfdac3d314561dcbf745
Junwei Pan <pandevirus@gmail.com>
2017-08-31 14:08:18 -0700

    Update split_train.py

--- python/split_train.py
+++ python/split_train.py
@@ -1,18 +1,24 @@
 import random
 
+# Split the criteo training data to train/validation/test data sets.
 path = '/tmp/jwpan/data_cretio/train.txt.thres20.yx'
-ratio = 0.7
+train_ratio = 0.6
+validation_ratio = 0.2
 
 random.seed(19941030)
 
-file1 = open(path + '.' + str(ratio), 'w')
-file2 = open(path + '.' + str(1-ratio), 'w')
+file1 = open(path + '.train', 'w')
+file2 = open(path + '.validation', 'w')
+file3 = open(path + '.test', 'w')
 
 for line in open(path):
-    if random.random() < ratio:
+    ran = random.random()
+    if ran < train_ratio:
         file1.write(line)
-    else:
+    elif ran < train_ratio + validation_ratio:
         file2.write(line)
+    else:
+        file3.write(line)
 
 file1.close()
 file2.close()
--- python/transfer.py
+++ python/transfer.py
@@ -1,27 +1,42 @@
 import sys
 
-# Transfer the original cretio dataset to libsvm format
-index_label = 28
-#lst_index_cat = range(14, 14 + 26)
-lst_index_cat = range(15)
-#num_field = 26
-#offset_train = 14
-#offset_test = 13
-thres = 10
+'''
+This script will do the following tasks:
+    1. Generate a feature index file which maps each feature to an unique index.
+    2. Transfer the original tsv files to libsvm format.
+'''
 
-print "index_label", index_label
-print "lst_index_cat", lst_index_cat
+# Configuration for Criteo data set
+index_label = 0
+lst_index_cat = range(14, 14 + 26)
+num_field = 26
+thres = 20
+
+# Configuration for Yahoo data set
+'''
+index_label = 28
+lst_index_cat = range(26)
+num_field = 15
+thres = 10
+'''
+
+print "Index of label", index_label
+print "List of indexes of categorical features", lst_index_cat
+print "Number of fields", num_field
+print "Threshold", thres
 
 d_field_fea = {}
 d_fea_index = {}
 d_field_fea_cnt = {}
 #path_train = '/tmp/jwpan/data_cretio/train.txt'
 #path_train = '../data_yahoo/ctr_20170524_0530_0.003.txt'
-path_train = '../data_yahoo/ctr_20170517_0530_0.015.txt'
+#path_train = '../data_yahoo/ctr_20170517_0530_0.015.txt'
+path_train = '../data_cretio/train.txt'
 #path_test = '../data_yahoo/ctr_20170531.txt.downsample_all.0.1'
-path_test = '../data_yahoo/ctr_20170601.txt.downsample_all.0.1'
+#path_test = '../data_yahoo/ctr_20170601.txt.downsample_all.0.1'
 #path_fea_index = '../data_yahoo/featindex_3m_thres' + str(thres) + '.txt'
-path_fea_index = '../data_yahoo/featindex_25m_thres' + str(thres) + '.txt'
+#path_fea_index = '../data_yahoo/featindex_25m_thres' + str(thres) + '.txt'
+path_fea_index = '../data_cretio/featindex_thres20.txt'
 batch = 100000
 
 def get_lines_of_file(path):
@@ -31,6 +46,11 @@ def get_lines_of_file(path):
     return n
 
 def build_field_feature(path, mode):
+    '''
+    Output:
+        d_field_fea, key: field index, value: set of features of a field
+        d_field_fea_cnt, key: field index, value: {key: feature index, value: count of samples with this feature}
+    '''
     total = get_lines_of_file(path)
     for i, line in enumerate(open(path)):
         if i % batch == batch - 1:
@@ -68,8 +88,10 @@ def create_fea_index(path):
     file.close()
 
 def create_yx(path, mode):
-    # There is some samples whose all features are rare(# < thres), 
-    # we need to filter all these samples, use cnt_filter as the counter.
+    '''
+    There is some samples whose all features are rare(# < thres), 
+    we need to filter all these samples, use cnt_filter as the counter.
+    '''
     cnt_qualify = 0
     cnt_filter = 0
     total = get_lines_of_file(path)
@@ -114,6 +136,6 @@ print 'create fea index'
 create_fea_index(path_fea_index)
 
 print 'create yx'
-#create_yx(path_train, 'train')
-create_yx(path_test, 'train')
+create_yx(path_train, 'train')
+#create_yx(path_test, 'train')
 #create_yx(path_test, 'test')
